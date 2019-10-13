c8a208730383fe4f7f347b1e89c6bdc68448fb7c
wachaon <wachaon@gmail.com>
2019-10-14 01:30:45 +0900

    Show url

--- example/google.js
+++ example/google.js
@@ -15,12 +15,19 @@ app( ( ie, event, result, wait ) => {
     ie.Document.getElementsByName( 'btnK' )[0].click()
     result.searchResult = []
 
+    let count = 0
+
+    event.on( /.+/, ( url ) => {
+        let colors = [ console.ansi.red, console.ansi.green, console.ansi.yellow, console.ansi.blue, console.ansi.magenta, console.ansi.cyan ]
+        console.log( `%s${ url }`, colors[ count++ % colors.length ] )
+    } )
+
     event.on( /^https:\/\/www.google.com\/search\?/, ( url ) => {
         const searchResult = Array.from( ie.Document.querySelectorAll( '.rc' ) )
         searchResult.forEach( node => {
             result.searchResult.push( Array.from( node.querySelectorAll( '.s' ) )[0].textContent + '\n' )
         } )
-        wait( 2000 )
+        // wait( 2000 )
         // ie.Quit()
         // throw new Error( 'ie.Quit()' )
     } )
